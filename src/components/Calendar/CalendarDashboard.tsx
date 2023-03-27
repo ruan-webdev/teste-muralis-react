d3350c1c05bd8a10e07f3f5556599c5f696c00f5
Ruan Campos <ruancamposdemarcos@gmail.com>
2023-03-27 19:58:19 -0300

    fix: calendar

--- src/components/Calendar/CalendarDashboard.tsx
+++ src/components/Calendar/CalendarDashboard.tsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import { CalendarWrapper } from './styles';
 
+type CalendarDashboardProps = {
+  date: Date;
+  view: string;
+};
 
 const CalendarDashboard = () => {
   const [date, setDate] = useState(new Date());
 
-  const handleDateChange = (newDate) => {
+  const handleDateChange = (newDate: React.SetStateAction<Date>) => {
     setDate(newDate);
   };
 
-  const tileClassName = ({ date, view }) => {
+  const tileClassName = ({ date, view }: CalendarDashboardProps ) => {
     // Verifica se o dia atual é igual ao dia renderizado
     if (view === 'month' && date.toDateString() === new Date().toDateString()) {
       return 'current-day';
@@ -21,7 +25,7 @@ const CalendarDashboard = () => {
 
   return (
     <CalendarWrapper>
-      <Calendar value={date} onChange={handleDateChange} showNavigation={false} tileClassName={tileClassName} />
+      <Calendar value={date} onChange={ ()=> handleDateChange} showNavigation={false} tileClassName={tileClassName} />
     </CalendarWrapper>
   );
 };
